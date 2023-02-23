b275828508681b17daeb2e035ddcca59aa94656a
Dmitry Sokolov <xx.sokolov@gmail.com>
2023-02-23 21:51:37 +0300

    Правки

--- classes/api.py
+++ classes/api.py
@@ -5,15 +5,39 @@ from classes.logger import Log
 from pydantic import BaseModel, Field
 
 
-class sendMessage(BaseModel):
-    chat_id: str = Field(default="", title='Chat ID', description="Укажите @username или Chat Name", include_in_schema=True)
-    text: str = Field(default="", title='Text', description="The description of the item", max_length=300)
-    disable_web_page_preview: bool = False
-    disable_notification: bool = False
+class send(BaseModel):
+    title: str = Field(title='Chat ID', description="Укажите @username или Chat Name", include_in_schema=True)
+    period: str = Field(title='Text', description="The description of the item")
+    header: str = Field(title='Text', description="The description of the item")
+    body: str = Field(title='Text', description="The description of the item")
 
+class macros(BaseModel):
+    hostname: str = Field(default='Zabbix server', title='Text', description="The description of the item")
+    itemid: str = Field(default='23271 *UNKNOWN* *UNKNOWN* *UNKNOWN*', title='Text', description="The description of the item")
+    hostid: str = Field(default='10084', title='Text', description="The description of the item")
+    triggerid: str = Field(default='13486', title='Text', description="The description of the item")
+    triggerurl: str = Field(default='Zabbix server', title='Text', description="The description of the item")
+    eventtags: str = Field(default='target:zabbix, ZNTMentions:@xxsokolov', title='Text', description="The description of the item")
+    eventid: str = Field(default='55', title='Text', description="The description of the item")
+    actionid: str = Field(default='7', title='Text', description="The description of the item")
 
-class FileItemBase(BaseModel):
-    current_project: str = "Test project"
+class znt(BaseModel):
+    graphs: bool = True
+    hostlinks: bool = True
+    graphlinks: bool = True
+    acklinks: bool = True
+    eventlinks: bool = True
+    triggerlinks: bool = True
+    eventtag: bool = True
+    eventidtag: bool = True
+    itemidtag: bool = True
+    triggeridtag: bool = True
+    actionidtag: bool = True
+    hostidtag: bool = True
+    zntsettingstag: bool = True
+    zntmentions: bool = True
+    keyboard: bool = True
+    graphs_period: str = 'default'
 
 
 api = FastAPI(debug=True)
@@ -28,9 +52,14 @@ def run(host="0.0.0.0", port=80):
 
 
 @v1.post("/telegram/sendMessage", status_code=status.HTTP_202_ACCEPTED)
-def read_item(item: sendMessage):
-    logger.info('cool')
-    return item
+def read_item(
+        chat_id: str,
+        send: send,
+        macro: macros,
+        znt: znt
+):
+    results = {"state": 'Ok'}
+    return results
 
 # znt = FastAPI()
 # v1 = FastAPI()
