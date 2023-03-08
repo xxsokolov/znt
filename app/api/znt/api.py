33d0107acdc234fe84ea260d859c5f8c5deac913
Dmitry Sokolov <xx.sokolov@gmail.com>
2023-03-08 22:50:58 +0300

    Правки

--- app/api/znt/api.py
+++ app/api/znt/api.py
@@ -6,12 +6,14 @@
 ########################
 # https://github.com/xxsokolov/znt
 from fastapi import APIRouter
+from app.api.znt.endpoints.healthcheck import healthcheck_router
 from app.api.znt.endpoints.bot import bot_router
 from app.api.znt.endpoints.proxy import proxy_router
 from app.api.znt.endpoints.chat import chat_router
 
 znt_router = APIRouter()
 
+znt_router.include_router(healthcheck_router, tags=['znt.healthcheck'])
 znt_router.include_router(bot_router, tags=['znt.bot'])
 znt_router.include_router(proxy_router, tags=['znt.proxy'])
 znt_router.include_router(chat_router, tags=['znt.chat'])
---
+++ app/api/znt/endpoints/healthcheck.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+########################
+#    Sokolov Dmitry    #
+# xx.sokolov@gmail.com #
+#  https://t.me/ZbxNTg #
+########################
+# https://github.com/xxsokolov/znt
+from fastapi import APIRouter, status
+
+healthcheck_router = APIRouter()
+
+
+@healthcheck_router.get("/healthcheck/", status_code=status.HTTP_200_OK, summary="Состояние сервиса")
+async def healthcheck():
+    return {"status": "ok"}
